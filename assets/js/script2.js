d25118b9e0aaaee00e40135c40da94e106954aba
codelove <abdurrahmaanbica123@gmail.com>
2024-12-15 15:28:24 +0100

    Added a page for the info about myself. added a new css styling on a new file for the about me page. added a new script for the typewriter effect that is being applied on the text of the about me page.

---
+++ assets/js/script2.js
@@ -0,0 +1,17 @@
+function typewriterEffect(elementId, text, speed) {
+    let i = 0;
+    let element = document.getElementById(elementId);
+    
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i); // Adding the next letter
+            i++;
+            setTimeout(type, speed); 
+        }
+    }
+    
+    type();
+}
+
+// Function call to start typing effect.
+typewriterEffect('typewriter-text', 'Hello, my name is Abdurrahmaan, a Mechatronics Engineering student. In my first year, I struggled with Java and could not even write a basic Hello World program. Determined to improve, I turned to online tutorials and practiced daily until I passed all my programming courses. Mastering any skill, like coding, requires consistent practice and discipline.', 50);
