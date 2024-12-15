860191497fca17ae300b8c73befceced3ec01f03
codelove <abdurrahmaanbica123@gmail.com>
2024-12-15 15:52:52 +0100

    Added a typing sound to be played when the typewriter effect negins.

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,17 +1,24 @@
-function typewriterEffect(elementId, text, speed) {
+ // Function for the typewriter effect
+ function typewriterEffect(elementId, text, speed) {
     let i = 0;
     let element = document.getElementById(elementId);
-    
+    let typingSound = new Audio('assets/audio/typing-sound.mp3'); 
+    typingSound.loop = true; 
+
     function type() {
         if (i < text.length) {
-            element.innerHTML += text.charAt(i); // Adding the next letter
+            element.innerHTML += text.charAt(i); // Adds the next letter
             i++;
+            typingSound.play(); 
             setTimeout(type, speed); 
+        } else {
+            typingSound.pause(); // Stop the sound when typing is finished
+            typingSound.currentTime = 0; // Reset the sound to the beginning
         }
     }
-    
-    type();
+
+    type(); // Start typing effect
 }
 
-// Function call to start typing effect.
+// Call the function to start the typewriter effect
 typewriterEffect('typewriter-text', 'Hello, my name is Abdurrahmaan, a Mechatronics Engineering student. In my first year, I struggled with Java and could not even write a basic Hello World program. Determined to improve, I turned to online tutorials and practiced daily until I passed all my programming courses. Mastering any skill, like coding, requires consistent practice and discipline.', 100);
